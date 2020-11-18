fd4783c5aa1378fff40754366755f511cc5cde54
Takuya Matsuyama <hi@craftz.dog>
2020-11-18 15:35:31 +0900

    fix(images): remove importImages

--- index.js
+++ index.js
@@ -58,43 +58,8 @@ function getMetaFromMarkdown(fn) {
   return meta
 }
 
-/**
- * Import images from given Markdown data with the specified base path
- *
- * @param {string} md - The Markdown content
- * @param {string} basePath - The base path to the Markdown file
- * @returns {string} The modified Markdown with attachment images
- */
-async function importImages(md, basePath) {
-  const { nativeImage } = require('electron')
-  const { models } = require('inkdrop')
-  const { File: IDFile } = models
-  const images = extractImages(md)
-  for (const image of images) {
-    const imagePath = path.resolve(basePath, image.url)
-    if (fs.existsSync(imagePath)) {
-      try {
-        const imageData = nativeImage.createFromPath(imagePath)
-        const file = await IDFile.createFromNativeImage(imageData)
-        const imageRegex = new RegExp(
-          escapeRegExp(`![${image.alt}](${image.url})`),
-          'g'
-        )
-        md = md.replace(imageRegex, `![${image.alt}](inkdrop://${file._id})`)
-      } catch (e) {
-        inkdrop.notifications.addError('Failed to import an image', {
-          detail: `${imagePath}: ${e.message}`,
-          dismissable: true
-        })
-      }
-    }
-  }
-  return md
-}
-
 module.exports = {
   getTitleAndBodyFromMarkdown,
   getMetaFromMarkdown,
-  importImages,
   extractImages
 }
